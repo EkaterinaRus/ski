4fab14202ab96fb7c3af808f417b34ed4bc38e75
EkaterinaRus <kateivan.89@mail.ru>
2023-04-03 11:35:40 +0300

    Добавление файлов

--- css/js/script.js
+++ css/js/script.js
@@ -1,4 +1,5 @@
 const searchBth = document.querySelector('.search-btn')
+const cancelBth = document.querySelector('.cancel-btn')
 const searchInput = document.querySelector('.search-input')
 const searchBox= document.querySelector('.search-box')
 const main = document.querySelector('main')
@@ -7,13 +8,23 @@ searchBth.addEventListener('click', function (e) {
     e.stopPropagation();
     searchBox.classList.add('active');
     searchInput.classList.add('active');
+    cancelBth.classList.add('active');
 });
 
-main.addEventListener('click', function (e) {
-    e.stopPropagation();
-    searchBox.classList.remove('active');
-    searchInput.classList.remove('active');
+
+document.addEventListener('click', function (e) {
+    if (e.target !== searchBox) {
+        searchBox.classList.remove('active');
+        searchInput.classList.remove('active');
+        cancelBth.classList.remove('active');
+    }
 });
+
+// main.addEventListener('click', function (e) {
+//     e.stopPropagation();
+//     searchBox.classList.remove('active');
+//     searchInput.classList.remove('active');
+// });
 
 
 
